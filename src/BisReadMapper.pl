87066fe8b50df2a26064515ca666b51bcc3f031b
Dinh Diep <dinh@genomeMiner>
2017-09-12 18:25:59 -0700

    fix bug for missing my in variable declaration

--- src/BisReadMapper.pl
+++ src/BisReadMapper.pl
@@ -255,8 +255,8 @@ sub sortsam{
 			}elsif($score == $last_score){
 				#check if one is from alternate reference (alternate references are not listed in fai file)
 				#two equivalent good hits, increment last_cnt
-				$last_ref = $last_fields[2];
-				$ref = $fields[2];
+				my $last_ref = $last_fields[2];
+				my $ref = $fields[2];
 				$last_ref =~ s/_Crick//;
 				$last_ref =~ s/_Watson//;
 				$ref =~ s/_Crick//;
